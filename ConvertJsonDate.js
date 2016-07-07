686930a95af7f611da74a8ffb09cb28262508367
Phạm Tý <phamty113@gmail.com>
2016-07-07 10:15:18 +0700

    Upload note

---
+++ ConvertJsonDate.js
@@ -0,0 +1,6 @@
+function ToJavaScriptDate(value) {
+    var pattern = /Date\(([^)]+)\)/;
+    var results = pattern.exec(value);
+    var dt = new Date(parseFloat(results[1]));
+    return new Date(dt.getFullYear(), dt.getMonth(), dt.getDate());
+}
